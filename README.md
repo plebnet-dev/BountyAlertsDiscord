e4c1312fbe84696561417ec94b83537b116f6b2d
bitc0indad <tobias.mele@gmail.com>
2024-02-18 18:30:30 -0500

    Changes to be commited:
        modified:   README.md
        new file:   chromedriver-max-x64/chromedriver-mac-x64/LICENSE.chromedriver
        new file:   chromedriver-max-x64/chromedriver-mac-x64/chromedriver
        new file:   sphinx_webscrape.py

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # BountyAlertsDiscord
-"BountyAlertsDiscord" is a tool designed to send notifications about new bounty opportunities directly to Discord channels. 
+
+"BountyAlertsDiscord" is a tool designed to send notifications about new bounty opportunities directly to Discord channels.
+
+## Overview
+
+This Python program automates the process of scraping web pages for bounty information and posting updates directly to Discord channels. It uses Selenium for web scraping to gather details about bounties from `https://community.sphinx.chat/bounties` and then pushes these updates to a specified Discord webhook.
+
+## Features
+
+- **Web Scraping**: Extracts bounty information, including author, description, price, currency, and URL.
+- **Discord Integration**: Automatically posts bounty details to a Discord channel via webhooks.
+
+## Requirements
+
+- Python 3
+- Selenium
+- Requests
+
+## Setup
+
+1. Install dependencies: `pip install selenium requests`.
+2. Update `chromedriver_path` with the path to your ChromeDriver.
+3. Set your Discord webhook URL in `webhook_url`.
+
+## Usage
+
+Run the script with `python3 sphinx_webscrape.py`. Ensure your system has ChromeDriver installed and accessible.
+
+## Contribution
+
+Feel free to fork, modify, and make pull requests to improve the script or add new features.
---
+++ sphinx_webscrape.py
@@ -0,0 +1,89 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+import time
+
+def scrape_bounties(driver):
+    url = 'https://community.sphinx.chat/bounties'
+    driver.get(url)
+    
+    wait = WebDriverWait(driver, 10)  # Adjust timeout as needed
+    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.DescriptionContainer")))  # Ensure this waits for a reliably present element
+
+    bounty_details = []
+
+    bounty_elements = driver.find_elements(By.CSS_SELECTOR, "div[data-testid='description-price-container']")  # Update this selector based on your page structure
+
+    for bounty in bounty_elements:
+        try:
+            author = bounty.find_element(By.CSS_SELECTOR, "div.sc-pzMyG.dnxgfw").text
+        except Exception:
+            author = "Unknown"  # Default value in case of an error
+
+        try:
+            href = bounty.find_element(By.CSS_SELECTO,"a").get_attribute('href')
+            url = f"https://community.sphinx.chat{href}" 
+        except Exception:
+            url = "Unknown"  # Default value in case of an error
+
+        try:
+            price = bounty.find_element(By.CSS_SELECTOR, "div[class*='Price_inner_Container'] div[class*='Price_Dynamic_Text']").text
+        except Exception:
+            price = "Unknown"  # Default value in case of an error
+
+        try:
+            currency = bounty.find_element(By.CSS_SELECTOR, "div[class*='Price_SAT_Container'] div[class*='Price_SAT_Text']").text
+        except Exception:
+            currency = "Unknown"  # Default value in case of an error
+
+        try:
+            posted_time = bounty.find_element(By.CSS_SELECTOR, "div.sc-pHIBf.eYPEZX").text
+        except Exception:
+            posted_time = "Unknown"  # Default value in case of an error
+
+        try:
+            description = bounty.find_element(By.CSS_SELECTOR, "div[class*='DescriptionContainer']").text
+        except Exception:
+            description = "Description not found"
+
+        bounty_details.append({
+            "author": author,
+            "posted_time": posted_time,
+            "description": description,
+            "currency": currency,
+            "price": price,
+            "url" : url
+        })
+
+    return bounty_details
+
+def post_to_discord(bounty_details, webhook_url):
+    for bounty in bounty_details:
+        message = f"{bounty.get('author', 'Unknown')} posted a bounty: {bounty.get('description', 'N/A')} {bounty.get('posted_time', 'N/A')} {bounty.get('price', 'N/A')} {bounty.get('currency', 'N/A')} {bounty.get('url', 'N/A')}"
+        payload = {"content": message}
+        response = requests.post(webhook_url, json=payload)
+        if response.status_code == 204:
+            print("Successfully posted to Discord.")
+        else:
+            print(f"Failed to post to Discord: {response.status_code}")
+
+def main():
+    chromedriver_path = '/Users/tobiasmele/Code_Projects/6_git/plebnet.dev/webscraping/chromedriver-max-x64/chromedriver-mac-x64'
+    service = Service(executable_path=chromedriver_path)
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        bounty_details = scrape_bounties(driver)
+        if bounty_details:
+            webhook_url = 'https://discord.com/api/webhooks/1208452918156460073/YiNa5U_WdDdInOIVe9Q8SiowwAHpR_kHZ5mYySWVldcm9BybeUXd9eqLhZD-BpAfpLFt'
+            post_to_discord(bounty_details, webhook_url)
+        else:
+            print("No bounty descriptions found.")
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
